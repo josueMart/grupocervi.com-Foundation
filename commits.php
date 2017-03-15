c8015ba90ff06cb6f21a2d2e732cc341d092f343
Josue Martinez <josue.mart@live.com.mx>
2017-03-15 10:48:32 -0600

    Se inicializan los widgets para tenerlos disponibles en el menu de wordpress

---
+++ commits.php
@@ -0,0 +1,47 @@
+commit 4b288d2908bef81f099cf7aeee25501742593d03
+Author: Josue Martinez <josue.mart@live.com.mx>
+Date:   Wed Mar 15 10:35:15 2017 -0600
+
+    preparacion de archivos para ecommerce
+
+commit 3cd303695d8a10c8e8c3efca6bedf2c1ceb5a9fb
+Author: Josue Martinez <josue.mart@live.com.mx>
+Date:   Sun Mar 12 09:23:31 2017 -0600
+
+    Terminado del frontend de la plantilla Partes y Consumibles
+
+commit b7e98854195c15cca644dcb6b381f09bf37fb70a
+Author: Josue Martinez <josue.mart@live.com.mx>
+Date:   Fri Mar 3 14:03:52 2017 -0600
+
+    Se rediseña single.php
+
+commit d0262b408cddc9d78821664ddbeca9f9dad3ad05
+Author: Josue Martinez <josue.mart@live.com.mx>
+Date:   Fri Mar 3 13:23:40 2017 -0600
+
+    Finalización del diseño de single.php
+
+commit 713d790d98d3b344ab3ad4d79c1298e7449eac1d
+Author: Josue Martinez <josue.mart@live.com.mx>
+Date:   Fri Mar 3 11:17:56 2017 -0600
+
+    Finzalizacion de index, diseño aprobado
+
+commit f6d63c02c57fa4902e3638913995ab393dd9780e
+Author: Josue Martinez <josue.mart@live.com.mx>
+Date:   Wed Mar 1 14:18:31 2017 -0600
+
+    Se termina el diseño para el header y footer
+
+commit ce69317d26a004ae547e99ce9d5d1da8e5be5118
+Author: Josue Martinez <josue.mart@live.com.mx>
+Date:   Sat Feb 25 13:37:15 2017 -0600
+
+    Creo la maquetacion del sitio web, agrego menus de wordpress y le doy contenido al footer
+
+commit 67ae3731b18153347c087e1437ad7719ac03ece9
+Author: Josue Martinez <josue.mart@live.com.mx>
+Date:   Fri Feb 24 17:08:12 2017 -0600
+
+    agrego wordpress y archivos iniciales de foundation
--- functions.php
+++ functions.php
@@ -13,6 +13,20 @@ function register_my_menus(){
         );
         
 }
+
+function arphabet_widgets_init() {
+
+    register_sidebar( array(
+        'name'          => 'Home right sidebar',
+        'id'            => 'home_right_1',
+        'before_widget' => '<div>',
+        'after_widget'  => '</div>',
+        'before_title'  => '<h2 class="rounded">',
+        'after_title'   => '</h2>',
+    ) );
+
+}
+add_action( 'widgets_init', 'arphabet_widgets_init' );
 add_action('init', 'register_my_menus');
 
 //-----------------------------Menus---------------------------------------------------
