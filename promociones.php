fa7eefa3b01d711c58499d87bd32b554d22d37c2
Josue Martinez <josue.mart@live.com.mx>
2017-06-12 13:28:28 -0500

    Se corrigen los errores de diseño de las promociones. Quedan listos

--- promociones.php
+++ promociones.php
@@ -1,4 +1,136 @@
-<?php //Template Name: Promociones ?>
-<?php get_header(); ?>
+<?php //Template Name: Promociones 
+get_header();
+?>
 
+   <div class="row" align="center" id="promo-title">
+       <h2>¡No te pierdas nuestras promociones!</h2>
+   </div>
+   <div class="row">
+     <div class="column large-3 medium-4 small-12 column-promo">
+       <div class="row img-promo" >
+         <img src="http://localhost/wp-content/uploads/2017/04/versacare-hill-rom.jpg" alt="">
+       </div>
+       <div class="row fam-promo">
+         <p>Equipo médico</p>
+       </div>
+        <div class="row marca-promo">
+         <p>Welch Allyn</p>
+       </div>
+        <div class="row modelo-promo">
+         <p>U-3100</p>
+       </div>
+        <div class="row precio-promo">
+         <p>$267.40</p>
+       </div>
+        <div class="row extract-promo">
+         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit modi fuga ipsum adipisci dolorum, pariatur accusamus neque asperiores ea corporis illum eveniet, numquam est rem voluptate iusto similique commodi nostrum?</p>
+       </div>
+       <div class="row terminos-promo">
+         <p>
+           *Precios sujetos a cambio sin previo aviso. <br>
+           *Precio Neto M.N. 
+         </p>
+       </div>
+       <div class="row btn-promo" align="center">
+          <a href="#" class="button">Más Información</a>
+         
+       </div>
+     </div>
+     
+     <!-- Articulo 2 -->
+  <div class="column large-3 medium-4 small-12 column-promo">
+       <div class="row img-promo" >
+         <img src="http://localhost/wp-content/uploads/2017/04/versacare-hill-rom.jpg" alt="">
+       </div>
+       <div class="row fam-promo">
+         <p>Equipo médico</p>
+       </div>
+        <div class="row marca-promo">
+         <p>Welch Allyn</p>
+       </div>
+        <div class="row modelo-promo">
+         <p>U-3100</p>
+       </div>
+        <div class="row precio-promo">
+         <p>$267.40</p>
+       </div>
+        <div class="row extract-promo">
+         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit modi fuga ipsum adipisci dolorum, pariatur accusamus neque asperiores ea corporis illum eveniet, numquam est rem voluptate iusto similique commodi nostrum?</p>
+       </div>
+       <div class="row terminos-promo">
+         <p>
+           *Precios sujetos a cambio sin previo aviso. <br>
+           *Precio Neto M.N. 
+         </p>
+       </div>
+       <div class="row btn-promo" align="center">
+          <a href="#" class="button">Más Información</a>
+         
+       </div>
+     </div>
+     
+     <!-- Articulo 3 -->
+      <div class="column large-3 medium-4 small-12 column-promo">
+       <div class="row img-promo" >
+         <img src="http://localhost/wp-content/uploads/2017/04/versacare-hill-rom.jpg" alt="">
+       </div>
+       <div class="row fam-promo">
+         <p>Equipo médico</p>
+       </div>
+        <div class="row marca-promo">
+         <p>Welch Allyn</p>
+       </div>
+        <div class="row modelo-promo">
+         <p>U-3100</p>
+       </div>
+        <div class="row precio-promo">
+         <p>$267.40</p>
+       </div>
+        <div class="row extract-promo">
+         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit modi fuga ipsum adipisci dolorum, pariatur accusamus neque asperiores ea corporis illum eveniet, numquam est rem voluptate iusto similique commodi nostrum?</p>
+       </div>
+       <div class="row terminos-promo">
+         <p>
+           *Precios sujetos a cambio sin previo aviso. <br>
+           *Precio Neto M.N. 
+         </p>
+       </div>
+       <div class="row btn-promo" align="center">
+          <a href="#" class="button">Más Información</a>
+         
+       </div>
+     </div>
+
+          <!-- Articulo 4 -->
+      <div class="column large-3 medium-4 small-12 column-promo">
+       <div class="row img-promo" >
+         <img src="http://localhost/wp-content/uploads/2017/04/versacare-hill-rom.jpg" alt="">
+       </div>
+       <div class="row fam-promo">
+         <p>Equipo médico</p>
+       </div>
+        <div class="row marca-promo">
+         <p>Welch Allyn</p>
+       </div>
+        <div class="row modelo-promo">
+         <p>U-3100</p>
+       </div>
+        <div class="row precio-promo">
+         <p>$267.40</p>
+       </div>
+        <div class="row extract-promo">
+         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit modi fuga ipsum adipisci dolorum, pariatur accusamus neque asperiores ea corporis illum eveniet, numquam est rem voluptate iusto similique commodi nostrum?</p>
+       </div>
+       <div class="row terminos-promo">
+         <p>
+           *Precios sujetos a cambio sin previo aviso. <br>
+           *Precio Neto M.N. 
+         </p>
+       </div>
+       <div class="row btn-promo" align="center">
+          <a href="#" class="button">Más Información</a>
+         
+       </div>
+     </div>
+   </div>   
 <?php get_footer(); ?>
