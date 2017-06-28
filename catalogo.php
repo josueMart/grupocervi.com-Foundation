34fc79eca8f71adb19ed4cad029a31324646789b
Josue Martinez <josue.mart@live.com.mx>
2017-06-28 16:50:28 -0500

    Creo los accesos directos para equipo medico y refacciones y partes

---
+++ catalogo.php
@@ -0,0 +1,14 @@
+<?php 
+// Template Name: Catalogo
+get_header();  ?>
+
+<div class="row fondo-blanco">
+	<div class="column large-6 medium-5 small-12 equipo-medico" align="center">
+		<a href="http://localhost/equipo-medico">Equipo Médico</a>
+	</div>
+	<div class="column large-6 medium-5 small-12 partes-consumibles" align="center">
+	<a href="http://localhost/partes-y-consumibles">Partes y Consumibles</a> 
+	</div>
+</div>
+
+ <?php  get_footer(); ?>
