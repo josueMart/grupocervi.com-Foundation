6ca9731e423ed855ab0fd416d3579f17e0a4a8ae
Josue Martinez <josue.mart@live.com.mx>
2017-06-28 16:56:52 -0500

    Se quita el plugin de slide para la creación del slide con foundation

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
   
 <!-- FILA SLIDER-->
 
-  <div class="row">
-  <?php wd_slider(2); ?>
+  <div class="row slide">
+
   </div>
   <!-- FIN FILA SLIDER-->
     
@@ -29,7 +29,7 @@
               <article>
               <h4><?php the_title(); ?></h4>
               <p><?php the_excerpt(); ?></p>
-              <a href="<?php the_permalink(); ?>" class="button">Leer más.</a>
+              <a href="<?php the_permalink(); ?>" class="button">Más información</a>
             </article>
             </div>
             
@@ -49,34 +49,66 @@
   <!-- FILA INTERMEDIA-->
 
    <div class="row acerca-row" align="center">
-   <h4 class="title-index">Acerca de Grupo Cervi</h4>
-   <p class="acerca-index">Somos un corporativo ubicado en la ciudad de Aguascalientes Ags. México, dedicado a la Importación, venta, renta y servicio de equipos médicos, así como a la venta de sus partes, consumibles y refacciones. Contamos con mas de 15 años de experiencia en el equipamiento de hospitales a nivel nacional. <a href="#">Conoce nuestro catálogo de productos.</a></p>
+   <h4>Acerca de Grupo Cervi</h4>
+   <p>Somos un corporativo ubicado en la ciudad de Aguascalientes Ags. México, dedicado a la Importación, venta, renta y servicio de equipos médicos, así como a la venta de sus partes, consumibles y refacciones. Contamos con mas de 15 años de experiencia en el equipamiento de hospitales a nivel nacional. <a href="#">Conoce nuestro catálogo de productos.</a></p>
 
    </div>
     <!-- BARRA DE OFERTAS -->
+      
     <div class="row promotions-row">
-    <!-- the loop -->
-    <?php 
+   <!-- ARTICULOS -->
+
+      <?php 
       $args = array('cat' => 10);
       $category_posts = new WP_Query($args);
       if($category_posts->have_posts()) :
       while($category_posts->have_posts()) :
     ?> 
-         <div class="column small-12  medium-3 large-3 promotions-column" align="center">
-          <?php 
+
+      <div class="column large-3 medium-4 small-12 column-promo" align="center">
+      <?php 
             $category_posts->the_post();
           ?>
-            <div class="row">
-              <?php the_post_thumbnail(); ?>
-            </div>
-            <div class="row"><article>              
-              <h4 class="promotion-cost"><?php the_title(); ?></h4>
-              <?php the_excerpt(); ?>
-              <a href="<?php the_permalink(); ?>" class="button" >Mas información</a>
-            </article></div>
-            
+         <div class="row img-promo" >
+        <?php the_post_thumbnail(); ?>
+       </div>
+       <div class="row brand-detail">
+          <div class="column large-6 " align="center">
+               <div class="row fam-promo">
+                 <p><?php the_title(); ?></p>
+               </div>
+               <div class="row marca-promo">
+                  <p><?php echo the_field("marca"); ?></p>
+               </div>
+               <div class="row modelo-promo">
+                 <p><?php echo the_field("modelo"); ?></p>
+               </div>
+       
+           </div>
+           <div class="column large-6 logo-marca" align="center">
+              <img src=" <?php echo the_field("imagen_marca"); ?>" alt=""> 
           </div>
-          <?php 
+
+
+       </div>
+        <div class="row precio-promo">
+         <p><?php echo the_field("precio") ?></p>
+       </div>
+        <div class="row extract-promo">
+         <p><?php the_excerpt(); ?></p>
+       </div>
+       <div class="row terminos-promo">
+         <p>
+           *Precios sujetos a cambio sin previo aviso. <br>
+           *Precio Neto <?php echo the_field("moneda") ?> 
+         </p>
+       </div>
+       <div class="row btn-promo" align="center">
+          <a href="<?php the_permalink(); ?>" class="button">Más Información</a>
+         
+       </div>
+      </div>
+       <?php 
            endwhile;
             else :
           ?>
@@ -85,13 +117,10 @@
          endif;
         wp_reset_postdata();
         ?>
-    <!-- fin the loop -->      
+
+
   </div>
-  <!-- FIN BARRA DE OFERTAS -->
-  <div class="column row btn-expand" align="center">
-    <a class="button expanded">Catálogo de productos</a>  
-  </div>
-   
+  <!-- FIN BARRA DE OFERTAS --> 
 </div>
 <?php get_footer(); ?>
     
