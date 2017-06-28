de2e7adc890c2194689a1331170585fc42a6347e
Josue Martinez <josue.mart@live.com.mx>
2017-06-28 16:52:31 -0500

    Queda establecido el contacto, instalando el plugin

--- contacto.php
+++ contacto.php
@@ -3,31 +3,13 @@
  ?>
 
  <?php get_header(); ?>
- 	<div class="row">
- 		imagen
- 	</div>
- 	<div class="row">
- 	
- 		<h2> Con mucho gusto atenderemos tus dudas, solicitiudes de información y/o cotizaciones.</h2>
- 	</div>
- 	<div class="row">
- 		<form action="enviar.php" method="post"> 			
- 				<div class="column medium-6">
- 					<input type="text" name="nombre" placeholder="Ingresa tu nombre" required>
- 					<input type="email" name="email" placeholder="Ingresa tu email" required>
- 					<input type="tel" name="telefono" placeholder="Ingresa tu telefono" required>
- 					<input type="text" name="asunto" placeholder="Ingresa el asunto" required>
- 					<label>
- 						Detalle del asunto.
- 						<textarea name="mensaje" placeholder="None" required></textarea>
- 					</label>
- 					<input type="submit" class="button expanded"> Enviar </button>
- 					
- 				</div>
- 				<div class="column medium-6"> Imagen</div>
- 			
- 			
- 		</form>
- 	</div>
+ 	<div class="row contact">
+ 		<h3> Con mucho gusto atenderemos tus dudas, <br> solicitiudes de información y/o cotizaciones.</h3>
 
+ 		<div class="column large-6 medium-11 small-10 large-centered small-centered medium-centered">
+ 			<?php echo do_shortcode('[contact-form-7 id="244" title="Contact form 1"]'); ?>
+
+
+ 		</div>
+ 	</div>
  <?php get_footer(); ?>
